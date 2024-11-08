57a50ee0f6e60c87d750fb840b406153689d7c7c
Derek Reynolds <superDreynolds@gmail.com>
2024-11-08 11:52:27 -0600

    Add Screen Manager (& readd transition at end of level 1)
    - Enemies now have their screens as their parent transforms
    - "Wave" behavior and other settings can be configured in the screen manager
    - Screens now have cube meshes to allow for the inverse of those meshes to be used as colliders when the player enters

--- Assets/Scripts/ColliderTrigger.cs
+++ Assets/Scripts/ColliderTrigger.cs
@@ -5,39 +5,38 @@ using UnityEngine.Events;
 
 public class ColliderTrigger : MonoBehaviour
 {
-    public bool oneShot = false;
     private bool alreadyEntered = false;
     private bool alreadyExited = false;
 
     public string collisionTag;
     public UnityEvent onTriggerEnter;
     public UnityEvent onTriggerExit;
+    public UnityEvent onTriggerEnterOneShot;
+    public UnityEvent onTriggerExitOneShot;
 
     private void OnTriggerEnter(Collider collision)
     {
-        if (alreadyEntered)
-            return;
-
         if (!string.IsNullOrEmpty(collisionTag) && !collision.CompareTag(collisionTag))
             return;
 
         onTriggerEnter?.Invoke();
 
-        if (oneShot)
-            alreadyEntered = true;
+        if (!alreadyEntered)
+            onTriggerEnterOneShot?.Invoke();
+
+        alreadyEntered = true;
     }
 
     private void OnTriggerExit(Collider collision)
     {
-        if (alreadyExited)
-            return;
-
         if (!string.IsNullOrEmpty(collisionTag) && !collision.CompareTag(collisionTag))
             return;
 
         onTriggerExit?.Invoke();
 
-        if (oneShot)
-            alreadyExited = true;
+        if (!alreadyExited)
+            onTriggerExitOneShot?.Invoke();
+
+        alreadyExited = true;
     }
 }
---
+++ Assets/Scripts/Managers/ScreenManager.cs
@@ -0,0 +1,236 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    public bool loadChildrenButton;
+    public bool blockUntilWavesClear = true;
+    public List<Spawn> spawns;
+    public List<Wave> waves;
+    public string tagToSpawn = "Enemy";
+
+    private int activeSpawns;
+    private int activeWaveIndex;
+
+    private bool wavesClear = false;
+
+    private void OnValidate()
+    {
+        if(loadChildrenButton)
+        {
+            LoadChildrenIntoSpawns();
+            loadChildrenButton = false;
+        }
+        PopulateWaves();
+    }
+
+    private void Start()
+    {
+        PrepareScreen();
+    }
+
+    public void StartScreen()
+    {
+        PrepareWaveStart(0);
+        if(blockUntilWavesClear)
+        {
+            if(TryGetComponent(out ScreenColliders screenColliders))
+            {
+                screenColliders.TurnOnColliders();
+            }
+            else
+            {
+                Debug.LogWarning(gameObject.name + "'s ScreenManager has blockUntilWavesClear set to true, but it has no ScreenColliders component!");
+            }
+        }
+    }
+
+    public void SpawnDefeated()
+    {
+        activeSpawns--;
+        if(activeSpawns < 0)
+        {
+            Debug.LogWarning(gameObject.name + "'s ScreenManager has an activeSpawn count of " + activeSpawns + "!");
+        }
+        // if the next wave starts when a number of spawns remain, check to see if we should start it yet
+        if (waves.Count > activeWaveIndex + 1 && waves[activeWaveIndex + 1].type == WaveType.StartWhenNumberOfSpawnsRemain)
+        {
+            if (activeSpawns <= waves[activeWaveIndex + 1].value)
+            {
+                StartWave(activeWaveIndex + 1);
+            }
+        }
+        else if (waves.Count <= activeWaveIndex + 1)
+        {
+            if (activeSpawns <= 0)
+            {
+                WavesClear();
+            }
+        }
+    }
+
+    private void PrepareScreen()
+    {
+        // hide spawns
+        foreach (Spawn spawn in spawns)
+        {
+            spawn.toSpawn.SetActive(false);
+        }
+        activeSpawns = 0;
+        AssignSpawnsToWaves();
+    }
+
+    private void WavesClear()
+    {
+        wavesClear = false;
+        if (blockUntilWavesClear && TryGetComponent(out ScreenColliders screenColliders))
+        {
+            screenColliders.TurnOffColliders();
+        }
+    }
+
+    private void LoadChildrenIntoSpawns()
+    {
+        if (spawns == null)
+        {
+            spawns = new List<Spawn>();
+        }
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject child = transform.GetChild(i).gameObject;
+            if (child.tag == tagToSpawn && spawns.FindAll(x => x.toSpawn == child).Count == 0)
+            {
+                spawns.Add(new Spawn() { toSpawn = transform.GetChild(i).gameObject, wave = 1, instantiateNew = false });
+            }
+        }
+    }
+
+    private void PopulateWaves()
+    {
+        if(spawns == null)
+        {
+            return;
+        }
+        int highestSpawnWave = 0;
+        foreach (Spawn spawn in spawns)
+        {
+            if (spawn.wave > highestSpawnWave)
+            {
+                highestSpawnWave = spawn.wave;
+            }
+        }
+        if (highestSpawnWave == waves.Count)
+        {
+            // all good
+        }
+        else if (highestSpawnWave < waves.Count)
+        {
+            // too many waves
+            while (highestSpawnWave < waves.Count)
+            {
+                waves.RemoveAt(waves.Count - 1);
+            }
+        }
+        else if (highestSpawnWave > waves.Count)
+        {
+            // not enough waves
+            while (highestSpawnWave > waves.Count)
+            {
+                waves.Add(new Wave() { number = waves.Count + 1, type = WaveType.StartImmediately, value = 0, spawns = null });
+            }
+        }
+    }
+
+    private void AssignSpawnsToWaves()
+    {
+        // PopulateWaves() should have already been called automatically, so we have the waves we need
+        for (int i = 0; i < waves.Count; i++)
+        {
+            waves[i].spawns = new List<Spawn>();
+        }
+        foreach (Spawn spawn in spawns)
+        {
+            waves[spawn.wave - 1].spawns.Add(spawn);
+        }
+    }
+
+    private void PrepareWaveStart(int waveIndex) // make sure to not call this method twice for any given wave!!!
+    {
+        if(waves.Count <= waveIndex) // we have no more waves
+        {
+            if (activeSpawns <= 0)
+            {
+                WavesClear();
+            }
+            return;
+        }
+        if (waves[waveIndex].type == WaveType.StartImmediately)
+        {
+            StartWave(waveIndex);
+        }
+        else if (waves[waveIndex].type == WaveType.WaitForSeconds)
+        {
+            StartCoroutine("StartWaveAfterSeconds", waves[waveIndex].value);
+        }
+        else if (waves[waveIndex].type == WaveType.StartWhenNumberOfSpawnsRemain)
+        {
+            if (activeSpawns <= waves[waveIndex].value)
+            {
+                StartWave(waveIndex);
+            }
+        }
+    }
+
+    private void StartWave(int waveIndex)
+    {
+        activeWaveIndex = waveIndex;
+        foreach (Spawn spawn in waves[waveIndex].spawns)
+        {
+            if (spawn.instantiateNew)
+            {
+                Instantiate(spawn.toSpawn, transform.position, Quaternion.identity);
+            }
+            else
+            {
+                spawn.toSpawn.SetActive(true);
+            }
+            activeSpawns++;
+        }
+        PrepareWaveStart(waveIndex + 1);
+    }
+
+    private IEnumerable StartWaveAfterSeconds(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        StartWave(activeWaveIndex + 1);
+    }
+
+    [Serializable]
+    public struct Spawn
+    {
+        public GameObject toSpawn;
+        public int wave;
+        public bool instantiateNew;
+    }
+
+    [Serializable]
+    public class Wave
+    {
+        public int number;
+        public WaveType type;
+        [InspectorName("Number of Seconds or Spawns")]
+        public int value;
+        [HideInInspector]
+        public List<Spawn> spawns { get; set; }
+    }
+
+    public enum WaveType
+    {
+        StartImmediately,
+        WaitForSeconds,
+        StartWhenNumberOfSpawnsRemain
+    }
+}
